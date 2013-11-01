a028c6544e47dbaff8db9aa74be2333b592577dd
jeremychung <jeremy.chung@live.com>
2013-11-01 07:55:06 -0700

    Added TimelineVisualizer stub

---
+++ src/Visualizer/TimelineVisualizer.java
@@ -0,0 +1,18 @@
+package Visualizer;
+
+import RepositoryContent.Author;
+
+public class TimelineVisualizer {
+
+	public TimelineVisualizer() {
+	}
+	
+	public void visualize(Author author) {
+		// Show visualizations for a single author
+	}
+	
+	public void visualizeAll() {
+		// Show visualizations for all authors
+	}
+	
+}
